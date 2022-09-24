6103d865296024e961b1bc75dadad2bcec0ff696
AnonymousPro <mayurathavale.code100@protonmail.com>
2022-09-25 00:49:45 +0530

    Concepts understood

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
+from importlib import resources
 from flask import Flask, request
-from flask_restplus import Api, Resource, fields
+from flask_restplus import Api , Resource , fields
 
 flask_app = Flask(__name__)
 app = Api(app = flask_app, 
-		  version = "1.0", 
-		  title = "Name Recorder", 
-		  description = "Manage names of various users of the application")
+				vesion = "1.0",
+				title = "Name Recorder",
+				description = "Manage names of various users of the application")
 
 name_space = app.namespace('names', description='Manage names')
 
 model = app.model('Name Model', 
-				  {'name': fields.String(required = True, 
-    					  				 description="Name of the person", 
-    					  				 help="Name cannot be blank.")})
+					{'name' : fields.String(required = True,
+											description = "Name of the person",
+											help = "Name cannot be blank.")})
 
 list_of_names = {}
 
@@ -47,3 +48,5 @@ class MainClass(Resource):
 			name_space.abort(500, e.__doc__, status = "Could not save information", statusCode = "500")
 		except Exception as e:
 			name_space.abort(400, e.__doc__, status = "Could not save information", statusCode = "400")
+
+		
